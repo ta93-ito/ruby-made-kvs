cb4068b128644e607f6109d209014b1e5aa0fb73
ta93-ito <tkm.ito@outlook.jp>
2021-12-13 19:44:30 +0900

    tmp

--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@
 
 require './server'
 
-server_start
+BasicServer.new
--- server.rb
+++ server.rb
@@ -2,12 +2,56 @@
 
 require 'socket'
 
-def server_start
-  server = TCPServer.new(3000)
-  loop do
-    Thread.start(server.accept) do |client|
-      client.puts "Hello! Time is #{Time.now}"
+class BasicServer
+
+  COMMANDS = [
+    "GET",
+    "SET",
+  ]
+
+  def initialize
+    @data_store = {}
+
+    server = TCPServer.new 3000
+    puts "Server started at: #{Time.now}"
+    loop do
+      client = server.accept
+      puts "New client connected: #{ client }"
+      client_command_with_args = client.gets
+      if client_command_with_args && client_command_with_args.strip.length > 0
+        response = handle_client_command(client_command_with_args)
+        client.puts response
+      else
+        puts "Empty request received from #{ client }"
+      end
       client.close
+    end
+  end
+
+  private
+
+  def handle_client_command(client_command_with_args)
+    command_parts = client_command_with_args.split
+    command = command_parts[0]
+    args = command_parts[1..-1]
+    if COMMANDS.include?(command)
+      if command == "GET"
+        if args.length != 1
+          "(error) ERR wrong number of arguments for '#{ command }' command"
+        else
+          @data_store.fetch(args[0], "(nil)")
+        end
+      elsif command == "SET"
+        if args.length != 2
+          "(error) ERR wrong number of arguments for '#{ command }' command"
+        else
+          @data_store[args[0]] = args[1]
+          'OK'
+        end
+      end
+    else
+      formatted_args = args.map { |arg| "`#{ arg }`," }.join(" ")
+      "(error) ERR unknown command `#{ command }`, with args beginning with: #{ formatted_args }"
     end
   end
 end
